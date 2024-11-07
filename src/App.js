ebd715b4a72e5e2dfb58db1f40e68ef62584bcf0
GiorgiGobb29 <ggobaze29@gmail.com>
2024-11-07 23:13:50 +0400

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Router, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
-
+import Home from "./components/Home/Home"
+import Main from "./components/Main/Main";
 function App() {
   return (
     <Routes>
-        <Route path="/" element={<div>123</div>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<div>Register</div>}/>
+        <Route path="/" element={<Login/>}/>
+        <Route element={<Main/>}>
+          <Route path="/Home" element={<Home/>}/>
+        </Route>
     </Routes>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export default function Main() {
+  return (
+    <div>
+      34
+      <Outlet/>
+    </div>
+  )
+}
